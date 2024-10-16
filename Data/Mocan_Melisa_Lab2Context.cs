39adcfedc463ed62ee7153d5328c33a097e249e5
Melisa <mocan.melisa.maria@gmail.com>
2024-10-16 20:31:33 +0300

    Add project files.

---
+++ Data/Mocan_Melisa_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mocan_Melisa_Lab2.Models;
+
+namespace Mocan_Melisa_Lab2.Data
+{
+    public class Mocan_Melisa_Lab2Context : DbContext
+    {
+        public Mocan_Melisa_Lab2Context (DbContextOptions<Mocan_Melisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mocan_Melisa_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
