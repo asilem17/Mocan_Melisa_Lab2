46adee0b65c8399ed6f754130f47663b3e7784c9
Melisa <mocan.melisa.maria@gmail.com>
2024-10-27 17:21:26 +0200

    Laborator3

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -63,6 +63,7 @@ namespace Mocan_Melisa_Lab2.Pages.Books
             }
             //se va include Author conform cu sarcina de la lab 2
             var bookToUpdate = await _context.Book
+             .Include(i=>i.Author)
             .Include(i => i.Publisher)
             .Include(i => i.BookCategories)
             .ThenInclude(i => i.Category)
