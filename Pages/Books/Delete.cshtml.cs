4ad50619c4f5684e9f5d850ea6c2b09e41af62ea
Melisa <mocan.melisa.maria@gmail.com>
2024-11-08 16:26:06 +0200

    lab5_done2

--- Pages/Books/Delete.cshtml.cs
+++ Pages/Books/Delete.cshtml.cs
@@ -29,7 +29,7 @@ namespace Mocan_Melisa_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book.Include(a=>a.Author).FirstOrDefaultAsync(m => m.ID == id);
 
             if (book == null)
             {
