2936f84f81c977f55f46fd217a519b96b8a2a76f
Melisa <mocan.melisa.maria@gmail.com>
2024-11-15 15:26:57 +0200

    Lab6

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,15 +8,18 @@ namespace Mocan_Melisa_Lab2.Models
     {
         public int ID { get; set; }
         [Display(Name = "Book Title")]
+        [StringLength(150, MinimumLength = 3)]
         public string Title { get; set; }
+        
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
 
-        [Column(TypeName = "decimal(6,2)")]
-
+        [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
+
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@ using Mocan_Melisa_Lab2.Data;
 using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+   policy.RequireRole("Admin"));
+});
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Publishers", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Categories", "AdminPolicy");
+});
 builder.Services.AddDbContext<Mocan_Melisa_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Mocan_Melisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Mocan_Melisa_Lab2Context' not found.")));
 
@@ -15,6 +29,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("Mocan_Melisa_Lab
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 options.SignIn.RequireConfirmedAccount = true)
+ .AddRoles<IdentityRole>()
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
